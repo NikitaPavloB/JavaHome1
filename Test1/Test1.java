9d06898a6096106b4dc9e205d4cf408eedaafc8d
Nikita <kindpavlov@yandex.ru>
2023-07-08 09:25:52 +0300

    Решил 1-ю задачу

--- Test1/Test1.java
+++ Test1/Test1.java
@@ -5,6 +5,30 @@ package Test1;
 public class Test1 {
 
   public static void main(String[] args) {
+    int n = 5; // Здесь можно указать любое значение n
 
+    int triangleNumber = calculateTriangleNumber(n);
+    System.out.println("Треугольное число " + n + " равно " + triangleNumber);
+
+    int factorial = calculateFactorial(n);
+    System.out.println("Факториал числа " + n + " равен " + factorial);
+  }
+
+  // Метод для вычисления треугольного числа
+  public static int calculateTriangleNumber(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+
+  // Метод для вычисления факториала числа
+  public static int calculateFactorial(int n) {
+    int factorial = 1;
+    for (int i = 1; i <= n; i++) {
+      factorial *= i;
+    }
+    return factorial;
   }
 }
