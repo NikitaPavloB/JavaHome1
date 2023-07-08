3508b66b866f5643bce789c0c266cfcec3eedb09
Nikita <kindpavlov@yandex.ru>
2023-07-08 09:31:42 +0300

    Решил 2-ю задачу

--- Test2/Test2.java
+++ Test2/Test2.java
@@ -5,6 +5,32 @@ package Test2;
 public class Test2 {
 
   public static void main(String[] args) {
-    
+    System.out.println("Простые числа от 1 до 1000:");
+    printPrimeNumbers(1, 1000);
+  }
+
+  // Метод для проверки, является ли число простым
+  public static boolean isPrime(int number) {
+    if (number <= 1) {
+      return false;
+    }
+
+    for (int i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  // Метод для вывода всех простых чисел в заданном диапазоне
+  public static void printPrimeNumbers(int start, int end) {
+    for (int number = start; number <= end; number++) {
+      if (isPrime(number)) {
+        System.out.print(number + " ");
+      }
+    }
+    System.out.println();
   }
 }
