6c8ecd821bd6b2060116a184a7404e5d454bd0a7
Nikita <kindpavlov@yandex.ru>
2023-07-08 09:53:46 +0300

    Решил 4-ю задачу

--- Test4/Test4.java
+++ Test4/Test4.java
@@ -4,8 +4,24 @@ package Test4;
 // Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
 
 public class Test4 {
-
   public static void main(String[] args) {
-    
+    boolean solutionFound = false;
+
+    for (int q = 0; q <= 9; q++) {
+      for (int w = 0; w <= 9; w++) {
+        for (int e = 0; e <= 9; e++) {
+          String expression = q + "" + w + " + " + w + "" + e + " = " + e + "" + e;
+          int result = q * 10 + w + w * 10 + e;
+          if (result == e * 10 + e) {
+            System.out.println("Решение найдено: " + expression);
+            solutionFound = true;
+          }
+        }
+      }
+    }
+
+    if (!solutionFound) {
+      System.out.println("Решение не найдено");
+    }
   }
 }
